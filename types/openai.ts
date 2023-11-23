9033bc7e1639d89146e2a8de24c5fa62b5cbcdd3
Ian Webster <ianw_github@ianww.com>
2023-11-23 08:46:46 -0800

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -8,10 +8,10 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo-0613',
+  GPT_3_5 = 'gpt-3.5-turbo-1106',
   GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4-0613',
-  GPT_4_32K = 'gpt-4-32k',
+  GPT_4 = 'gpt-4-1106-preview',
+  GPT_4_32K = 'gpt-4-1106-preview',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
